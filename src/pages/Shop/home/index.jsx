6269a6e87ca0a79f378dc826793138aa522d489e
Truong Pham <truongpham2412.nd@gmail.com>
2023-11-24 11:29:18 +0700

    ok

--- src/pages/Shop/home/index.jsx
+++ src/pages/Shop/home/index.jsx
@@ -1,11 +1,9 @@
-import CoreUploadFile from "../../../@core/components/inputs/CoreUploadFile";
 import ShopLayout from "../../../@core/layout/ShopLayout";
 
 const Home = (props) => {
   return (
     <ShopLayout>
       <>HOME PAGE</>;<>Upload file</>
-      <CoreUploadFile />
     </ShopLayout>
   );
 };
