52208b20c20dad9e201617a27b8f9bf2a93d81cc
Truong Pham <truongpham2412.nd@gmail.com>
2023-12-30 12:00:44 +0700

    cat code di

--- filekhonglienquan.js
+++ filekhonglienquan.js
@@ -1,5 +1,9 @@
 export const a = 1;
 
 // fix them cai bug bcd
-// hoom nay laf thuw 7 
-// Day laf code moi 
+// hoom nay laf thuw 7
+// Day laf code moi
+
+// lam tinh nang o day
+
+// lam tiep tinh nang
